4c2c70b831ba902abe83a80cd68d213d83ec220b
Diamondhesit <diamondheisttm@gmail.com>
2025-05-30 14:49:01 +0100

    fix: increase file upload size limit to 10mb

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,11 @@ const nextConfig: NextConfig = {
     domains: ['res.cloudinary.com'],
     unoptimized: true, 
   },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb'
+    }
+  }
 };
 
 export default nextConfig;
